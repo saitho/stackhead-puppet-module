6b0467d37306bb616ee4937a9d03b22004758df4
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-24 16:08:28 +0200

    feat(nginx/ssl_proxy): add server_name option

--- manifests/nginx/ssl_proxy.pp
+++ manifests/nginx/ssl_proxy.pp
@@ -1,17 +1,19 @@
 define stackhead::nginx::ssl_proxy (
   $proxy_port,
   $listen_port = 80,
+  $server_name = $name,
 ) {
   include nginx
 
   nginx::resource::server { "${name}":
-    ensure                => present,
-    ssl                   => true,
-    ssl_cert => false,
-    ssl_key => false,
-    ssl_redirect => true,
+    ensure          => present,
+    server_name     => "${server_name}",
+    ssl             => true,
+    ssl_cert        => false,
+    ssl_key         => false,
+    ssl_redirect    => true,
     proxy           => "http://127.0.0.1:${proxy_port}",
-    #ssl_cert              => '/path/to/wildcard_mydomain.crt',
+    #ssl_cert        => '/path/to/wildcard_mydomain.crt',
     #ssl_key               => '/path/to/wildcard_mydomain.key',
   }
 }
