a1469de2495bd55649352e7da5bd6bcc4bcde85e
Mario Lubenka <mario.lubenka@googlemail.com>
2020-07-27 18:53:33 +0200

    fix(nginx): syntax error

--- manifests/project/setup_container.pp
+++ manifests/project/setup_container.pp
@@ -13,10 +13,11 @@ define stackhead::project::setup_container (
   $domains.each |Hash $domain| {
     $domain[expose].each |Integer $index, Hash $expose| {
 
-      $auth_basic = 'security' in domain and 'authentication' in domain[security] ? {
-        true => domain[security][authentication].filter |$item| { $item[type] == 'basic' },
-        default => undef
+      $items = 'security' in domain and 'authentication' in domain[security] ? {
+        true => domain[security][authentication],
+        default => []
       }
+      $basicauth_items = $items.filter |$item| { $item[type] == 'basic' }
 
       stackhead::nginx::ssl_proxy { "${domain[domain]}-${expose[external_port]}":
         project_name         => $name,
@@ -25,8 +26,8 @@ define stackhead::project::setup_container (
         listen_port          => $expose[external_port],
         proxy_port           => $expose[internal_port],
         use_ssl              => $use_ssl,
-        auth_basic           => length($auth_basic) > 0 ? { true => $basicauth_title, default => undef},
-        auth_basic_user_file => length($auth_basic) > 0 ? { true => "${stackhead::htpasswd_path}/.${domain[domain]}", default => undef},
+        auth_basic           => $basicauth_items.length() > 0 ? { true => $basicauth_title, default => undef},
+        auth_basic_user_file => $basicauth_items.length() > 0 ? { true => "${stackhead::htpasswd_path}/.${domain[domain]}", default => undef},
       }
     }
   }
